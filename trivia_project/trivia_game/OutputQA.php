ef60d723789cce5b868d7cf69bd9d6f9e9ec76db
John Pepp <jrpepp@pepster.com>
2016-10-01 13:06:20 -0400

    Add files via upload

--- trivia_project/trivia_game/OutputQA.php
+++ trivia_project/trivia_game/OutputQA.php
@@ -14,28 +14,20 @@ class OutputQA {
     protected $q_num = \NULL;
     protected $query = \NULL;
     protected $stmt = \NULL;
+    protected $today = \NULL;
 
     public function __construct() {
         
     }
 
-    protected function buildQuizTables(array $categories) {
-        $db = DB::getInstance();
-        $this->pdo = $db->getConnection();
-        $this->query = "SELECT 1 FROM trivia_questions WHERE id > 0";
-        $this->stmt = $this->pdo->prepare($this->query);
-        $this->stmt->bindParam(':player', $this->player);
-        $this->stmt->execute();
-        $this->result = $this->stmt->fetch();
-    }
 
     public function readQA($q_num) {
         $db = DB::getInstance();
         $this->pdo = $db->getConnection();
-        $this->q_num = $q_num;
-        $this->query = "SELECT id, q_num, question, answer1, answer2, answer3, answer4, play_date FROM the_daily_ten WHERE q_num=:q_num";
+        $this->today = new DateTime("Now", new DateTimeZone("America/Detroit"));
+        $this->query = "SELECT id, q_num, question, answer1, answer2, answer3, answer4, play_date FROM trivia_questions WHERE q_num=:q_num AND day_of_year=:day_of_year";
         $this->stmt = $this->pdo->prepare($this->query);
-        $this->stmt->execute([':q_num' => $this->q_num]);
+        $this->stmt->execute([':q_num' => $q_num, ':day_of_year' => $this->today->format("z")]);
         $this->data = $this->stmt->fetchAll(PDO::FETCH_OBJ);
         if ($this->data) {
             return $this->data;
@@ -49,7 +41,7 @@ class OutputQA {
         $this->pdo = $db->getConnection();
         $this->today = new DateTime("Now", new DateTimeZone("America/Detroit"));
         $this->today->modify("Midnight");
-        $query = "SELECT  id, correct, play_date FROM the_daily_ten WHERE id =:id";
+        $query = "SELECT  id, correct, play_date FROM trivia_questions WHERE id =:id";
         $this->stmt = $this->pdo->prepare($query);
         $this->stmt->execute([':id' => $id]);
         $this->row = $this->stmt->fetch(PDO::FETCH_ASSOC);
