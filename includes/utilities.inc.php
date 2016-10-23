7bc3cd60a5087748d9db356e12221a959ea64092
John Pepp <jrpepp@pepster.com>
2016-10-23 07:54:23 -0400

    Add files via upload

--- includes/utilities.inc.php
+++ includes/utilities.inc.php
@@ -2,7 +2,7 @@
 /* Turn on error reporting */
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(-1);
+error_reporting(-1); // -1 = on || 0 = off
 
 date_default_timezone_set('America/Detroit');
 
